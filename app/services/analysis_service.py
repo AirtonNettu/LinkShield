938d4cc9ee6cd7ddb9d3ebf7a134c29e029e64e9
José Airton <200454915+AirtonNettu@users.noreply.github.com>
2026-08-02 01:58:59 -0300

    feat(service): Implement analysis pipeline flow

--- app/services/analysis_service.py
+++ app/services/analysis_service.py
@@ -1,12 +1,35 @@
 from app.repositories.analysis_repository import AnalysisRepository
+from app.repositories.url_repository import UrlRepository
 
-class AnalysisService: 
+from app.models.analise import Analise
+from app.models.url_analisada import UrlAnalisada
+
+class AnalysisService:
     """Coorderna o fluxo de análise de segurança."""
-    def __init__(self, analysis_repository: AnalysisRepository):
+    def __init__(
+            self,
+            analysis_repository: AnalysisRepository, 
+            url_repository: UrlRepository
+        ):
+        
         self.analysis_repository = analysis_repository
+        self.url_repository = url_repository
 
     def scan_url(self, url: str) -> str:
-        """Inicia a análise de segurança para uma URL fornecida."""
-        # Lógica para iniciar a análise de segurança
-        # Pode incluir chamadas a outros serviços, validação de URL, etc.
+        """Inicia o fluxo de análise de uma URL."""
+        url_analisada = UrlAnalisada(
+            usuario_id=1,
+            url=url,
+            dominio=url
+        )
+        self.url_repository.create_url(url_analisada)
+
+        analysis = Analise(
+            url_id=url_analisada.id,
+            status="pendente",
+            tipo_analise="url_scan"
+        )
+
+        self.analysis_repository.create_analysis(analysis)
+
         return url
